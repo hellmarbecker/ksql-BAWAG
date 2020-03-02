b00428c883d0135d3b652f9d4d93f531a7e851d0
Hellmar Becker <becker@hellmar-becker.de>
2020-03-02 18:02:25 +0100

    schema magic

--- bin/escape_schema.sh
+++ bin/escape_schema.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "%q" $(cat odl_movements.schema.json) >odl_movements.schema.json.escaped
+printf "%q" $(cat odl_movements.schema.json) | perl -pe 's/\\([\[\]{}])/$1/g' >odl_movements.schema.json.escaped
