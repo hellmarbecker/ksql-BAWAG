a627a528eb52f310f38427fbe2b9c6b868f7f60f
Hellmar Becker <becker@hellmar-becker.de>
2020-03-03 09:10:55 +0100

    removed connector for now, added delimiter logic to produce script

--- bin/produce.sh
+++ bin/produce.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kafka-console-producer --broker-list localhost:9092 --topic ODL_MOVEMENTS < data/in/ODL_MOVEMENTS_CONFLUENT_POC-2018-12-01-sample1000.csv
+sed -e 's/";"/","/g' ../data/in/ODL_MOVEMENTS_CONFLUENT_POC-2018-12-01-sample1000.csv | kafka-console-producer --broker-list localhost:9092 --topic ODL_MOVEMENTS_RAW
 
