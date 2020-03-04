4cedb3d2491ea60af1dacc3e15a4af92b78c5612
Hellmar Becker <becker@hellmar-becker.de>
2020-03-04 03:02:19 +0100

    script updated

--- bin/blowup.pl
+++ bin/blowup.pl
@@ -8,11 +8,17 @@ my $csv = Text::CSV->new({ sep_char => ';' });
 
 # read input file
 while (my $line = <>) {
-    chomp $line;
-
-    my @fields = split /;/, $line;
-    my $tstamp = $fields[0];
-    print "$tstamp\n";
+    # print $line;
+    if (my $status = $csv->parse($line)) {
+        my @f = $csv->fields();
+    } else {
+        print "status: $status\n";
+        my $bad_argument = $csv->error_input ();
+        print "bad_argument: $bad_argument\n";
+    }
+    #my @fields = split /;/, $line;
+    #my $tstamp = $fields[0];
+    #print "$tstamp\n";
  
 }
     
