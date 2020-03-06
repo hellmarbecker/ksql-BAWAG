92293ff88f1b9380dd051fac792d406322b6a85a
Hellmar Becker <becker@hellmar-becker.de>
2020-03-06 10:07:45 +0100

    .

---
+++ bin/make_comma.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use Text::CSV;
+my $csv_in = Text::CSV->new({ sep_char => ';' });
+my $csv_out = Text::CSV->new({ sep_char => ',' });
+
+# read input file
+while (my $line = <>) {
+
+    if (my $status = $csv_in->parse($line)) {
+
+        my @f = $csv_in->fields();
+        $csv_out->print(\*STDOUT, \@f);
+
+    } else {
+
+        print "status: $status\n";
+        my $bad_argument = $csv_in->error_input();
+        print "bad_argument: $bad_argument\n";
+   }
+
+}
+    
